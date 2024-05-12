e2074835cf5e92a339c0c221a348a7fbfb15df7f
waza <100469500+wazalvb@users.noreply.github.com>
2024-05-12 21:25:03 +0200

    user_db .sql

--- tfe/user_db .sql
+++ tfe/user_db .sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 11 mai 2024 à 12:57
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
+-- Généré le : Dim 12 mai 2024 à 19:24
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,14 +29,14 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `horaire_profs`;
 CREATE TABLE IF NOT EXISTS `horaire_profs` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `id_prof` int NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prof` int(11) NOT NULL,
   `jour` varchar(20) NOT NULL,
-  `heure` int NOT NULL,
+  `heure` int(11) NOT NULL,
   `classe` varchar(20) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `id_prof` (`id_prof`,`jour`,`heure`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `horaire_profs`
@@ -46,7 +46,7 @@ INSERT INTO `horaire_profs` (`ID`, `id_prof`, `jour`, `heure`, `classe`) VALUES
 (15, 15, 'lundi', 5, '6TTI'),
 (16, 15, 'lundi', 8, '6TTI'),
 (17, 15, 'mardi', 2, '6TTI'),
-(18, 15, 'lundi', 4, '6TTSEA');
+(18, 18, 'lundi', 4, '6TTSEA');
 
 -- --------------------------------------------------------
 
@@ -56,13 +56,13 @@ INSERT INTO `horaire_profs` (`ID`, `id_prof`, `jour`, `heure`, `classe`) VALUES
 
 DROP TABLE IF EXISTS `presences`;
 CREATE TABLE IF NOT EXISTS `presences` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `id_eleve` int NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `id_eleve` int(11) NOT NULL,
   `jour` varchar(20) NOT NULL,
   `etat` varchar(20) NOT NULL,
-  `heure` int NOT NULL,
+  `heure` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `presences`
@@ -96,28 +96,21 @@ INSERT INTO `presences` (`ID`, `id_eleve`, `jour`, `etat`, `heure`) VALUES
 
 DROP TABLE IF EXISTS `rapport`;
 CREATE TABLE IF NOT EXISTS `rapport` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `eleve` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `classe` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `rapport` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `eleve` varchar(100) NOT NULL,
+  `classe` varchar(20) NOT NULL,
+  `rapport` varchar(500) NOT NULL,
+  `type` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `rapport`
 --
 
 INSERT INTO `rapport` (`ID`, `eleve`, `classe`, `rapport`, `type`) VALUES
-(27, 'Bastien Dupont', '6TTI', 'frappe sur un éleve', ''),
-(28, 'Dary Caram', '4TTSE', 'crache sur la prof', ''),
-(33, 'Dary Caram', '4TTSE', 'eurghrofiyu', ''),
-(34, 'Dary Caram', '4TTSE', 'comment il va mon petit ?', ''),
-(36, 'Dary Caram', '6TTSEA', 'zaazdazd', 'disciplinaire'),
-(37, 'Bastien Dupont', '6TTI', 'azdzadazd', 'disciplinaire'),
-(39, 'Noa Claes', '6TTI', 'efzef', 'disciplinaire'),
-(40, 'Noa Claes', '6TTI', 'deze', 'disciplinaire'),
-(41, 'Lou Vanbe', '6TTI', 'ezfzeef', 'tenue_vestimentaire');
+(40, 'Noa Claes', '6TTI', 'Insulte un élève dans les couloirs', 'disciplinaire'),
+(41, 'Lou Vanbe', '6TTSEA', 'Habillé en jogging dans l\'école', 'tenue_vestimentaire');
 
 -- --------------------------------------------------------
 
@@ -127,22 +120,23 @@ INSERT INTO `rapport` (`ID`, `eleve`, `classe`, `rapport`, `type`) VALUES
 
 DROP TABLE IF EXISTS `sandwiches`;
 CREATE TABLE IF NOT EXISTS `sandwiches` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `eleve` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `classe` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `quantite` int NOT NULL,
-  `type` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `modif` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `approuve` int DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `eleve` varchar(200) NOT NULL,
+  `classe` varchar(100) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `modif` varchar(150) NOT NULL,
+  `approuve` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `sandwiches`
 --
 
 INSERT INTO `sandwiches` (`ID`, `eleve`, `classe`, `quantite`, `type`, `modif`, `approuve`) VALUES
-(6, 'Bastien Dupont', '6TTI', 3, 'usa', 'sans tomate ni oignons mais je veux bien gouda', 1);
+(6, 'Bastien Dupont', '6TTI', 3, 'usa', 'sans tomate ni oignons mais je veux bien gouda', 1),
+(7, 'Lou Vanbe', '6TTSEA', 1, 'usa', 'sans salade', NULL);
 
 -- --------------------------------------------------------
 
@@ -152,12 +146,12 @@ INSERT INTO `sandwiches` (`ID`, `eleve`, `classe`, `quantite`, `type`, `modif`, 
 
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE IF NOT EXISTS `student` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `prenom` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `nom` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `classe` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `classe` varchar(10) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `student`
@@ -177,27 +171,30 @@ INSERT INTO `student` (`ID`, `prenom`, `nom`, `classe`) VALUES
 
 DROP TABLE IF EXISTS `user_form`;
 CREATE TABLE IF NOT EXISTS `user_form` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `surname` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `user_type` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user',
-  `certification` int DEFAULT NULL,
-  `classe` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `matière` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `user_type` varchar(100) NOT NULL DEFAULT 'user',
+  `certification` int(11) DEFAULT NULL,
+  `classe` varchar(20) NOT NULL,
+  `matière` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `user_form`
 --
 
 INSERT INTO `user_form` (`id`, `name`, `surname`, `email`, `password`, `user_type`, `certification`, `classe`, `matière`) VALUES
-(11, 'mami', 'oups', 'mami@gmail.com', '$2y$10$UONVOPQ11jd92/innCx9U.9kl7/KllQ/mBsu1.bHGXV7ETVE8inba', 'admin', 1, '5TTE', 'sciences'),
 (15, 'Roussel', 'Jean-Marc', 'Roussel@gmail.com', '$2y$10$KoxUsqxOTDrHz1pGfx6UGuzThZCjpRMcQsU7SlK2zoPjxs9hw7LIK', 'user', 1, '6TTI', 'Informatique et labo logique/informatique'),
 (17, 'Roussel', 'Jean-Marc', 'Roussel_admin@gmail.com', '$2y$10$0J.PCRMkhLtgX3/h4Up7DO1lSu1dT1zwP1PoTlpfXUV1l4V6uvT2.', 'admin', 1, '', ''),
-(18, 'toto', 'toto', 'toto@gmail.com', '$2y$10$ftxDCLlCrS5C0n2j7obdRe777YQmjCLa3CfOs8MQN8Rm9336BTQwC', 'user', 1, '', '');
+(18, 'toto', 'titi', 'toto@gmail.com', '$2y$10$ftxDCLlCrS5C0n2j7obdRe777YQmjCLa3CfOs8MQN8Rm9336BTQwC', 'user', 1, '', ''),
+(19, 'Merlin', 'Marc', 'Merlin@gmail.com', 'Merlin', 'user', 1, '5TTI', 'TPT'),
+(20, 'Pirlot', 'Romy', 'Pirlot@gmail.com', 'pirlot', 'admin', 1, '', ''),
+(21, 'Van Douret', 'Julie', 'VanDouret@gmail.com', 'julie', 'user', 1, '6TTSEA', 'Mathématique'),
+(22, '', '', '', '(j\'ai inserré manuelement dans la db les mots de passe de certain utilisateurs, comme ça vous savez vou connectez avec ceux là) c\'est pour ça qu\'il y en a certain non hashé', 'user', NULL, '', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
